711bd4bcba7f0ffabae5393d75b610ea264a1c5e
bhoslan <bhoslan@gmail.com>
2021-07-19 15:37:41 +0300

    19 temmuz

---
+++ Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() {Id=1, Name="Engin", LastName="Demirog", City="Ankara" }; // Id Name PascalCase kullan
+            Customer customer2 = new Customer(2, "A", "B", "C");
+            Console.WriteLine(customer2.Name);
+        }
+    }
+
+    class Customer
+    {
+        public Customer() // parametresi olmayan constructor default constructor dır
+        {
+
+        }
+        public Customer(int id, string name, string lastName, string city ) //class ın ismiyle aynı ve geri dönüş tipi yok. new lendiğinde bu blok çalışır. 
+        {                                                                   //metot parametreleri camelCase yazılır
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            City = city;
+
+        }
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public String City { get; set; }
+    }
+}
